1a2af232e706e97bca0d88b793c54ce97d8dde40
lihui@accurant.bio <lihui@accurant.bio>
2020-09-25 12:44:36 +0800

    添加项目文件。

---
+++ C++_Primer_Chapter1/1.4.cpp
@@ -0,0 +1,12 @@
+/*
+本例感想：虽然不知道为什么1.2 return -1 没有报错，也不知道2020年了return 0是否还有必要，但在查清楚之前，还是保留return 0的习惯比较好。
+#include<iostream>
+
+int main()
+{
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << v1 * v2 << std::endl;
+	return 0;
+}
+*/
