c1f7607ce1db2bd7505cde99d58a7c354118bf19
Geeklihui <geeklihui@outlook.com>
2020-09-27 23:41:06 +0800

    完成第一章学习，此项目结束。

---
+++ C++_Primer_Chapter1/1.20.cpp
@@ -0,0 +1,12 @@
+/* 网站 http://www.informit.com/title/0321714113 （自动跳转到 https://www.informit.com/store/c-plus-plus-primer-9780321714114 ）上没有合适的头文件，只有 VS 2012 版本的，尝试了一下，调试的时候会闪退。以后有能力了再解决吧，这一题先跳过。同理 1.21 - 1.24 也跳过。
+* #include<iostream>
+* #include"Sales_item.h"
+* int main()
+* {
+*	Sales_item item1, item2;
+*	std::cin >> item1 >> item2;
+*	std::cout << item1 << item2 << std::endl;
+*	return 0;
+* }
+*/
+
---
+++ C++_Primer_Chapter1/testforoutfile.cpp
@@ -0,0 +1,10 @@
+/* 此例测试文件重定向功能。调试方法：项目属性、调试中添加命令参数： <test.txt >outtest.txt
+* #include<iostream>
+* int main()
+* {
+*	int a = 0, b = 0;
+*	std::cin >> a >> b;
+*	std::cout << a + b << std::endl;
+*	return 0;
+* }
+*/
